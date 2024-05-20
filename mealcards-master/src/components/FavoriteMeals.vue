03c21f29a8c4cb8d818eb4720a35bf69bd4c35b5
eli_0903 <91484965+Walts03@users.noreply.github.com>
2024-05-20 22:59:08 +1000

    update favmeal and home
    stylist home
    responsive fav meal

--- mealcards-master/src/components/FavoriteMeals.vue
+++ mealcards-master/src/components/FavoriteMeals.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="mt-20">
-    <h2 class="text-2xl font-bold mb-7 text-center justify-center">
-      Your Favorite Meals
-    </h2>
+  <div class="mt-20 container mx-auto px-4">
+    <h2 class="text-2xl font-bold mb-7 text-center">Your Favorite Meals</h2>
     <div v-if="favoriteMealsDetails.length === 0" class="text-center">
       <p>You have no favorite meals.</p>
     </div>
-    <div v-else>
-      <table class="min-w-full bg-white border border-gray-300">
+    <div v-else class="responsive-table">
+      <table class="w-full bg-white border border-gray-300">
         <thead>
           <tr>
             <th class="py-2 px-4 border-b">No.</th>
@@ -78,7 +76,34 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-.table-auto {
-  width: 100%;
+.container {
+  max-width: 1200px;
+}
+
+.responsive-table {
+  overflow-x: auto;
+}
+
+@media (max-width: 640px) {
+  .table th,
+  .table td {
+    padding: 10px; /* Adjust padding */
+    display: block; /* Stack the table cells */
+    text-align: right;
+  }
+
+  .table th::before {
+    float: left;
+    content: attr(aria-label); /* Use aria-label to replicate the thead */
+  }
+
+  .table td::before {
+    float: left;
+    content: attr(aria-label);
+  }
+
+  .text-2xl {
+    font-size: 1.5rem; /* Smaller font size for smaller screens */
+  }
 }
 </style>
